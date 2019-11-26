615985a011cc9a2f3fd83349c2471711542f7c6b
CarliNabbe <carli_nabbe@hotmail.com>
2019-11-26 13:52:12 +0100

    Added heroImage + button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
+import HeroImage from './components/HeroImage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Welcome to Greening!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeroImage />
     </div>
   );
 }
