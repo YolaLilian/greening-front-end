ef15fa48c8478b8eef86a1e8cb3252371da10297
CarliNabbe <carli_nabbe@hotmail.com>
2019-12-03 17:37:03 +0100

    Made progress on cardlinks. Image still missing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/navigation/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import HeroImage from './components/HeroImage';
+import Cardlink from './components/cardlink';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <HeroImage />
+      <Cardlink />
     </BrowserRouter>
   );
 }
---
+++ src/components/cardlink.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import cardlink from '../images/cardlink_bg.jpg'; // Use this image
+import icon_1 from '../images/brain.svg';
+import icon_2 from '../images/idea.svg';
+import icon_3 from '../images/united.svg';
+import icon_4 from '../images/magic-hat.svg';
+import '../App.css';
+
+class Cardlink extends Component {
+  constructor() {
+    super()
+    this.state = {
+      title: "Greening",
+    }
+    console.log("Hallo ik ben er")
+  }
+
+
+  //Dit is je render wat getoond wordt.
+  render() {
+    const container = {
+        height:"100vh",
+        backgroundColor: "white",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center"
+    }
+
+    const cardlinkStyle = {
+        backgroundImage:`url(${cardlink})`,
+        backgroundSize: "cover",
+        height:"150px",
+        width: "100px",
+        alignSelf: "center",
+        padding: "30px",
+        margin: "40px",
+        marginTop: "-300px",
+    }
+
+    const h3_style = {
+        color: "white",
+        textAlign: "center"
+    }
+    return (
+      <div className="App">
+          <div style={container}>
+              <div className="cardlink" style={cardlinkStyle}>
+                <img src={icon_1} />
+                <h3 style={h3_style}>Brainstormen</h3>
+              </div>
+              <div className="cardlink" style={cardlinkStyle}>
+                <img src={icon_2} />
+                <h3 style={h3_style}>Ideeen</h3>
+              </div>
+              <div className="cardlink" style={cardlinkStyle}>
+                <img src={icon_3} />
+                <h3 style={h3_style}>Bijeenkomst</h3>
+              </div>
+              <div className="cardlink" style={cardlinkStyle}>
+                <img src={icon_4} />
+                <h3 style={h3_style}>Tips and tricks</h3>
+              </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+
+export default Cardlink;
