9f5ec0ad3c6fd833e0424ca9d110b1c6e0530b28
CarliNabbe <carli_nabbe@hotmail.com>
2019-12-05 16:27:49 +0100

    Added change ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import Navbar from './components/navigation/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import HeroImage from './components/HeroImage';
 import Cardlink from './components/cardlink';
 import BrainstormCard from './components/BrainstormCard';
-=======
 import HeroImage from './components/hero/HeroImage';
->>>>>>> 8db345c885a4b2633240b8bacb83c2df617e3a2f
 
 function App() {
   return (
