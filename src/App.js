3b1c89eeec5256912447ab19a5a3152264c2d4a1
CarliNabbe <carli_nabbe@hotmail.com>
2019-12-05 16:17:01 +0100

    Added BrainstormCard component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/navigation/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import HeroImage from './components/HeroImage';
+import BrainstormCard from './components/BrainstormCard';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <HeroImage />
+      <BrainstormCard />
     </BrowserRouter>
   );
 }
