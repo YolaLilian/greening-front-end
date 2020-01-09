a4ab468738eb85f0747b4e318ec30e35ffee69d7
CarliNabbe <carli_nabbe@hotmail.com>
2020-01-09 14:45:20 +0100

    Added form css

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,27 +6,41 @@ const Form = (addIdea) => {
     const formik = useFormik({
         initialValues: {
             title: '',
-            description: '',
-            location: ''
+            subject: '',
+            description: ''
         },
         onSubmit: values => {
-            fetch("http://greening.louis.lol/api/idea", {
+            fetch("http://greening.louis.lol/api/ideas", {
                 method: "POST",
-                body: JSON.stringify(values)
+                body: JSON.stringify(values),
+                isSubmitting: true
             })
         },
     });
 
+    function handleClick() {
+        console.log("I was clicked")
+    }
+
     return (
         <div>
-            <form onSubmit={formik.handleSubmit}>
-                <input onChange={formik.handleChange} value={formik.values.title} name="title" />
-                <input onChange={formik.handleChange} value={formik.values.description} name="description" />
-                <input onChange={formik.handleChange} value={formik.values.location} name="location" />
-                <button type="submit">submit</button>
+            <h2 style={myStyle}>Voeg hier je idee toe!</h2>
+            <form onSubmit={formik.handleSubmit} style={myStyle}>
+                <input onChange={formik.handleChange} value={formik.values.title} name="title" placeholder="title" />
+                <input onChange={formik.handleChange} value={formik.values.subject} name="subject" placeholder="subject" />
+                <input onChange={formik.handleChange} value={formik.values.description} name="description" placeholder="description"/>
+                <button type="submit" onClick={handleClick}>submit</button>
             </form>
         </div>
     )
+}
+
+const myStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignSelf: "center",
+    flexDirection: "row",
+    padding: "30px"
 }
 
 export default Form
