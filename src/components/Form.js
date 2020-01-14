24ffbf356ecbcaa1da5564a7e299a57bd479acd3
Lilian Kempees <0926313@hr.nl>
2020-01-14 21:40:00 +0100

    Added visuals to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,72 +1,97 @@
-import React from 'react'
-import { useFormik } from 'formik';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import { useFormik } from "formik";
+import { Redirect } from "react-router-dom";
 
-const Form = (addIdea) => {
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //       title: "Greening",
-          
-    //     }
-
-    // loadData() {
-    //     <h1>{this.state.title}</h1>
-    //     this.setState( {title: "Je idee is opgeslagen!"})
-    //   }
-
+const Form = addIdea => {
     const formik = useFormik({
         initialValues: {
-            title: '',
-            subject: '',
-            description: ''
+            title: "",
+            subject: "",
+            description: ""
         },
         onSubmit: values => {
             fetch("http://greening.louis.lol/api/ideas", {
                 method: "POST",
                 body: JSON.stringify(values),
                 headers: {
-                    'Accept':'application/json',
-                    'Content-Type':'application/json',
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
                 }
-            })
-            return <Redirect to="/ideas" />
-        },
+            });
+            return <Redirect to="/ideas" />;
+        }
     });
-
-    const myStyle = {
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "row",
-        padding: "30px",
-    }
-
-    const formInput = {
-        borderStyle: "none",
-        backgroundColor: "#F3F3F3",
-        width: "80%",
-        flexDirection: "column",
-        display: "flex",
-        margin: "10px"
-    }
 
     const myStyleDiv = {
         backgroundColor: "white",
         width: "50vw",
         display: "flex",
         alignSelf: "center",
+        justifyContent: "center"
+    };
+
+    const myStyle = {
+        padding: "30px"
+    };
+
+    const formInput = {
+        borderStyle: "none",
+        backgroundColor: "#F3F3F3",
+        width: "80%",
+        flexDirection: "column",
+        margin: "10px",
+        padding: "10px 15px",
+        fontSize: "20px",
+        fontFamily: "Open Sans",
+        borderRadius: "25px"
+    };
+
+    const h1_style = {
+      color: "#3ba755",
+      padding: "0 30px"
+    };
+
+    const labelStyle = {
+      fontSize: "20px",
+      fontFamily: "Open Sans"
     }
 
     return (
         <div style={myStyleDiv}>
-            <form onSubmit={formik.handleSubmit} style={myStyle}>
-                <input onChange={formik.handleChange} style={formInput} value={formik.values.title} name="title" placeholder="title"/>
-                <input onChange={formik.handleChange} style={formInput} value={formik.values.subject} name="subject" placeholder="subject"/><br></br>
-                <input onChange={formik.handleChange} style={formInput} value={formik.values.description} name="description" placeholder="description"/>
-                <button type="submit">submit</button>
-            </form>
+            <h1 style={h1_style}>Idee creëeren</h1>
+            <div>
+                <form onSubmit={formik.handleSubmit} style={myStyle}>
+                    <label style={labelStyle}>Titel van idee</label>
+                    <input 
+                      onChange={formik.handleChange} 
+                      style={formInput} 
+                      value={formik.values.title} 
+                      name="title" 
+                      placeholder="titel" 
+                    />
+                    <br />
+                    <label style={labelStyle}>Onderwerp van idee</label>
+                    <input 
+                      onChange={formik.handleChange} 
+                      style={formInput} 
+                      value={formik.values.subject} 
+                      name="subject" 
+                      placeholder="onderwerp" 
+                    />
+                    <br></br>
+                    <label style={labelStyle}>Beschrijving van idee</label>
+                    <input
+                      onChange={formik.handleChange}
+                      style={formInput}
+                      value={formik.values.description}
+                      name="description"
+                      placeholder="beschrijving"
+                    />
+                    <button type="submit">Verzenden</button>
+                </form>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Form
+export default Form;
